f3c199bfda84fd4a47c376326ccfacf79e3ccd9e
Francis.dev <149237151+FrancisGil-dev@users.noreply.github.com>
2024-01-24 10:15:14 +0800

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//scrolltoView
+const scrollView = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) section.scrollIntoView({behavior: 'smooth'})  
+}
+//toggleDarkmode
+const toggle = document.getElementById('toggleIcon').addEventListener('click', () => {
+    const toggleIcon = document.getElementById('toggleIcon');
+    const body = document.body;
+    const logo = document.querySelectorAll('h1')[0].classList.toggle('text-white')
+    body.classList.add("transition-all");
+    body.classList.toggle("bg-[#272829]");
+    body.classList.toggle("text-white");
+    
+    toggleIcon.classList.remove();
+    toggleIcon.classList.toggle('bi-sun');
+
+
+})
