c4cecfbe7f083eea4d7612d660b3065bd0227a6d
Francis.dev <149237151+FrancisGil-dev@users.noreply.github.com>
2024-01-27 13:00:13 +0800

    commit

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 //scrolltoView
-const scrollView = (sectionId) => {
-    const section = document.getElementById(sectionId);
-    if (section) section.scrollIntoView({behavior: 'smooth'})  
-}
-//toggleDarkmode
-const toggle = document.getElementById('toggleIcon').addEventListener('click', () => {
-    const toggleIcon = document.getElementById('toggleIcon');
-    const body = document.body;
-    const logo = document.querySelectorAll('h1')[0].classList.toggle('text-white')
-    body.classList.add("transition-all");
-    body.classList.toggle("bg-[#272829]");
-    body.classList.toggle("text-white");
+const scrollView = () => {
+    //doms
+    const navMenu = document.querySelectorAll('li');
+    const section = document.querySelectorAll('section');
+    //function to scroll
+     const scroll = (sec) => sec.scrollIntoView({behavior: 'smooth'})
     
-    toggleIcon.classList.remove();
-    toggleIcon.classList.toggle('bi-sun');
+     //addEventListener
+     navMenu[0].addEventListener('click', () => scroll(section[0]))
+     navMenu[1].addEventListener('click', () => scroll(section[1]))
+     navMenu[2].addEventListener('click', () => scroll(section[2]))
+     navMenu[3].addEventListener('click', () => scroll(section[3]))
+
+     //addEventListener the toggleList in Small Screens
+}
+scrollView();
+
+//toggleIcon 
+const toggleIcon = document.querySelectorAll('i')[0].addEventListener('click', () => {
+    //get the toggleDiv Doms and Spans
+    const toggleList = document.getElementById("toggleList");
+    //then toggle
+    toggleList.classList.toggle("hidden")
+    toggleList.classList.toggle("flex")
+    
+    //for list scroll
+    const toggleScrollView = () => {
+    //get the doms
+        const toggleNavs = document.querySelectorAll('span');
+        const section = document.querySelectorAll('section');
+
+    //function to scroll
+        const scroll = (sec) => sec.scrollIntoView({behavior: 'smooth'})
+
+    //addEventListner the toggleNavs
+        toggleNavs[0].addEventListener('click', () => scroll(section[0]))
+        toggleNavs[1].addEventListener('click', () => scroll(section[1]))
+        toggleNavs[2].addEventListener('click', () => scroll(section[2]))
+        toggleNavs[3].addEventListener('click', () => scroll(section[3]))
+    }
+    toggleScrollView();
+   
+
 })
+
+
+
